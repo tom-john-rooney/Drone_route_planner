4bc3c60ff3dd900c12832125276fc0409f38c600
tom-john-rooney <tom.john.rooney@gmail.com>
2021-10-11 17:26:25 +0100

    Created menus class, fixed error handling

--- src/main/java/uk/ac/ed/inf/LongLat.java
+++ src/main/java/uk/ac/ed/inf/LongLat.java
@@ -11,37 +11,27 @@ import java.util.Objects;
 public class LongLat {
     /** The value which all longitudes must be strictly less than */
     public static final double MAX_LONGITUDE = -3.184319;
-
     /** The value which all longitudes must be strictly greater than */
     public static final double MIN_LONGITUDE = -3.192473;
-
     /** The value which all latitudes must be strictly less than */
     public static final double MAX_LATITUDE = 55.946233;
-
     /** The value which all longitudes must be strictly greater than */
     public static final double MIN_LATITUDE = 55.942617;
-
     /** The maximum distance allowed in degrees between two points for them to be 'close to' one another */
     public static final double DISTANCE_TOLERANCE = 0.00015;
-
     /** The number of which any drone move's direction in degrees must be a scalar multiple of */
     public static final int ANGLE_SCALE = 10;
-
     /** The largest bearing on which the drone can move */
     public static final int MAX_ANGLE = 350;
-
     /** The smallest bearing on which the drone can move */
     public static final int MIN_ANGLE = 0;
-
     /** The special 'junk' value to represent when the drone is hovering */
-    public static final int HOVERING_ANGLE = -999;
-
+    public static final int JUNK_ANGLE = -999;
     /** The length of any move the drone makes in degrees */
     public static final double MOVE_DISTANCE = 0.00015;
 
     /** The longitude component of the coordinate */
     public double longitude;
-
     /** The latitude component of the coordinate */
     public double latitude;
 
@@ -50,12 +40,8 @@ public class LongLat {
      *
      * @param longitude the longitude component of the coordinate being initialised
      * @param latitude  the latitude component of the coordinate being initialised
-     * @throws NullPointerException if any of the passed parameter values are null
      */
     public LongLat(double longitude, double latitude){
-        Objects.requireNonNull(longitude, "Longitude may not be null.");
-        Objects.requireNonNull(latitude, "Latitude may not be null.");
-
         this.longitude = longitude;
         this.latitude = latitude;
     }
@@ -78,12 +64,10 @@ public class LongLat {
      *
      * @param point to which the Pythagorean distance from the instance calling
      *              the method is calculated
-     * @throws NullPointerException if the coordinate passed is null
      * @return the Pythagorean distance between the calling instance and point
      */
     public double distanceTo(LongLat point){
-        Objects.requireNonNull(point, "Coordinate may not be null");
-
+        isPointNull(point);
         return Math.hypot(this.longitude - point.longitude, this.latitude - point.latitude);
     }
 
@@ -92,12 +76,10 @@ public class LongLat {
      *
      * @param point which is being checked to see if it is 'close to' the instance
      *              calling the method
-     * @throws NullPointerException if the LongLat instance passed is null
      * @return true if the instance calling the method is close to point, false otherwise
      */
     public boolean closeTo(LongLat point){
-        Objects.requireNonNull(point, "Coordinate may not be null");
-
+        isPointNull(point);
         return this.distanceTo(point) < DISTANCE_TOLERANCE;
     }
 
@@ -105,24 +87,27 @@ public class LongLat {
      * Calculates the next position of the drone after a move
      *
      * @param angle the direction in which the move is to be made in degrees
-     * @throws IllegalArgumentException if the angle provided is not a multiple of the ANGLE_SCALE scaling constant
-     *                                  or if the angle lies outside the range [0,350]
-     * @return a LongLat object representing the longitude and latitude of the drone after the move
+     * @return a LongLat object representing the longitude and latitude of the drone after the move,
+     *         this object is null if the angle is not a multiple of ANGLE_SCALE or lies outside the range
+     *         [MIN_ANGLE,MAX_ANGLE]
      */
     public LongLat nextPosition(int angle){
-        if(angle == HOVERING_ANGLE){
+        if(angle == JUNK_ANGLE){
             return new LongLat(this.longitude, this.latitude);
         }
-        else if(!(angle % ANGLE_SCALE == 0)){
-            throw new IllegalArgumentException(String.format("Drone can only travel on bearings that are a multiple of %d.",ANGLE_SCALE));
-        }
-        else if(angle < MIN_ANGLE || angle > MAX_ANGLE){
-            throw new IllegalArgumentException(String.format("Drone must fly on a bearing between %d and %d degrees inclusive.",MIN_ANGLE,MAX_ANGLE));
+        else if(!(angle % ANGLE_SCALE == 0) || angle > MAX_ANGLE || angle < MIN_ANGLE){
+            return null;
         }
         else{
             double newLongitude = this.longitude + MOVE_DISTANCE * Math.cos(Math.toRadians(angle));
             double newLatitude = this.latitude + MOVE_DISTANCE * Math.sin(Math.toRadians(angle));
             return new LongLat(newLongitude, newLatitude);
+        }
+    }
+
+    public void isPointNull(LongLat point){
+        if(point == null){
+            System.exit(1);
         }
     }
 }
---
+++ src/main/java/uk/ac/ed/inf/Menus.java
@@ -0,0 +1,14 @@
+package uk.ac.ed.inf;
+
+/**
+ *
+ */
+public class Menus {
+    public String machine;
+    public String port;
+
+    public Menus(String machine, String port){
+        this.machine = machine;
+        this.port = port;
+    }
+}
